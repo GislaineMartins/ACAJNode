32055acbd7de67873d649d244a9ba54a86667210
Gislaine Amancio <gimcjf@gmail.com>
2021-12-04 19:05:49 -0300

    Adicionando atividade 70

---
+++ atv70/calculadora/Calculadora.js
@@ -0,0 +1,26 @@
+export class Calculadora{
+    numeroUm;
+    numeroDois;
+
+
+    somar(numeroUm, numeroDois){
+        return numeroUm + numeroDois;
+    }
+
+    subtracao(numeroUm, numeroDois){
+        return numeroUm - numeroDois;
+    }
+
+    multiplicacao(numeroUm, numeroDois){
+        return numeroUm * numeroDois;
+    }
+
+    divisao(numeroUm, numeroDois){
+        return numeroUm / numeroDois;
+    }
+    
+    
+    
+
+
+}
---
+++ atv70/pessoa/Pessoa.js
@@ -0,0 +1,32 @@
+export class Pessoa{
+
+    nome;
+    cpf;
+    idade;
+
+   get nome(){
+       return this.nome;
+   }
+
+   set nome(nome){
+       this.nome = nome;
+   }
+
+   get cpf(){
+       return this.cpf;
+   }
+
+   set cpf(cpf){
+       this.cpf = cpf
+   }
+
+   get idade(){
+       return this.idade;
+   }
+
+   set idade(idade){
+       this.idade = idade;
+   }
+
+
+}
